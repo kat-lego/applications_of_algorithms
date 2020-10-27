565eebea449232c46b807b0d6e2adefb66da78d3
kat-lego <katlegomoses4@gmail.com>
2020-10-27 15:03:40 +0200

    something something bang bang

--- assignment5/ConvexHull.java
+++ assignment5/ConvexHull.java
@@ -15,8 +15,7 @@ public class ConvexHull {
 
     }
 
-    public static ArrayList<Point> GrahamScan(Point[] P){
-        Point[] Q = P.clone();
+    public static ArrayList<Point> GrahamScan(Point[] Q){
 
         Point p0 = Q[0];
         int min = 0;
@@ -44,56 +43,56 @@ public class ConvexHull {
         }
 
         if(new_size<3)return null;
-        ArrayList<Point>S = new ArrayList<>();
-        S.add(Q[0]);
-        S.add(Q[1]);
-        S.add(Q[2]);
+        ArrayList<Point>CH = new ArrayList<>();
+
+        for(int i=0; i<3; i++)CH.add(Q[i]);
 
         for (int i = 3; i < new_size; i++)
         {
-            while ( Point.orientation(S.get(S.size()-2), S.get(S.size()-1), Q[i]) != 2)
-                S.remove(S.size()-1);
-            S.add(Q[i]);
+            while ( Point.orientation(CH.get(CH.size()-2), CH.get(CH.size()-1), Q[i]) != 2)
+                CH.remove(CH.size()-1);
+            CH.add(Q[i]);
         }
 
-        return S;
+        return CH;
     }
 
     public static ArrayList<Point> JarvisMarch(Point[] Q){
         if (Q.length < 3) return null;
 
-        ArrayList<Point> hull = new ArrayList<>();
+        ArrayList<Point> CH = new ArrayList<>();
 
         int l = 0;
-        for (int i = 1; i < Q.length; i++)
-            if (Q[i].x < Q[l].x)
+        for (int i = 1; i < Q.length; i++){
+            if (Q[i].x < Q[l].x){
                 l = i;
+            }
+        }
 
         int p = l, q;
         do
         {
-            hull.add(Q[p]);
+            CH.add(Q[p]);
 
             q = (p + 1) % Q.length;
 
             for (int i = 0; i < Q.length; i++)
             {
-                if (Point.orientation(Q[p], Q[i], Q[q]) == 2)
-                    q = i;
+                if (Point.orientation(Q[p], Q[i], Q[q]) == 2) q = i;
             }
 
             p = q;
 
         } while (p != l);
 
-        return hull;
+        return CH;
     }
 
     public static void test(){
         boolean res = true;
         for(int i=0; i<1000; i++) {
             Point[] points = Point.genRandomPoints(1000);
-            ArrayList<Point> hull = GrahamScan(points);
+            ArrayList<Point> hull = GrahamScan(points.clone());
             ArrayList<Point> hull1 = JarvisMarch(points);
             if(!hull.containsAll(hull1)){
                 res = false;
@@ -105,33 +104,35 @@ public class ConvexHull {
 
     public static void analysis(int n_data, int step_size, int reruns){
         int n = 0;
-        double time1,time2,time3;
+        long time1,time2,time3, h;
         long start, end;
         for(int i=0; i<n_data; i++){
             n += step_size;
-            time1 = 0; time2 =0 ; time3 = 0;
+            time1 = 0; time2 =0 ; time3 = 0; h = 0;
 
             for(int r=0;r<reruns;r++){
                 Point[] points = Point.genRandomPoints(n);
+                Point[] points_copy = points.clone();
+
+                start = System.nanoTime();
+                ArrayList<Point> hull = GrahamScan(points_copy);
+                end = System.nanoTime();
+                time1 += end - start;
 
                 start = System.nanoTime();
                 ArrayList<Point> hull2 = JarvisMarch(points);
                 end = System.nanoTime();
                 time2 += end - start;
-
-                start = System.nanoTime();
-                ArrayList<Point> hull = GrahamScan(points);
-                end = System.nanoTime();
-                time1 += end - start;
                 
-                if(hull2.size()>n/2)System.out.println("hull big");
+                h += hull2.size();
 
-                time3 += (double)(end - start)/hull2.size();
+                time3 += (end - start)/hull2.size();
             }
             time1 = time1/(reruns*1000000);
             time2 = time2/(reruns*1000000);
             time3 = time3/(reruns*1000000);
-            System.out.format("%d, %f, %f, %f\n", n, time1, time2, time3);
+            h/=reruns;
+            System.out.format("%d, %d, %d, %d, %d\n", n, time1, time2, time3, h);
         }
 
     }
@@ -147,7 +148,6 @@ class PointComparator implements Comparator<Point> {
     @Override
     public int compare(Point p1, Point p2) {
 
-        // Find orientation
         int orientation = Point.orientation(p0, p1, p2);
         if (orientation == 0){
             return Double.compare(p0.euclidean(p1), p0.euclidean(p2));
@@ -161,6 +161,7 @@ class PointComparator implements Comparator<Point> {
 }
 
 class Point{
+    private static final int SCALE = 100000;
     double x, y;
     public Point(double x, double y){
         this.x = x;
@@ -187,8 +188,8 @@ class Point{
         double x, y;
 
         for(int i=0;i<n;i++){
-            x = (rand.nextDouble() - rand.nextDouble())*10000;
-            y = (rand.nextDouble() - rand.nextDouble())*10000;
+            x = (rand.nextDouble() - rand.nextDouble())*SCALE;
+            y = (rand.nextDouble() - rand.nextDouble())*SCALE;
             points[i] = new Point(x, y);
         }
 
@@ -196,9 +197,10 @@ class Point{
     }
 
     public static int orientation(Point p, Point q, Point r) {
-        double val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+        double cross = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
 
-        if (val == 0) return 0;  // colinear
-        return (val > 0)? 1: 2; // clock or counterclock wise
+        if (cross == 0) return 0;
+        else if(cross > 0) return 1;
+        else return 2;
     }
 }
--- assignment5/Readme.txt
+++ assignment5/Readme.txt
@@ -5,5 +5,8 @@
 | * Running                                             |
 |   - run 'make clean' to remove any build files        |
 |   - run 'make' to compile the code                    |
-|   - run 'make run' to run the code                    |
+|   - run 'make run' to run the code, the code executes |
+|     a function call test() that checks for            |
+|     correctness, followed by a function that runs the |
+|     simulations.                                      |
 +-------------------------------------------------------+
